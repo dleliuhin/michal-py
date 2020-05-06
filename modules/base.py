5a767d9a05e181fdece4777cee5c1b6c72a389f2
Dmitrii Leliuhin <dleliuhin@gmail.com>
2020-05-06 10:29:45 +0300

    Feature. HoughTransform finish.

--- modules/base.py
+++ modules/base.py
@@ -1,16 +1,30 @@
-
 class Result(object):
 
-    def __init__(self, top, bottom, left, right):
+    def __init__(self, top=None, bottom=None, left=None, right=None):
         self.top = top
         self.bottom = bottom
         self.left = left
         self.right = right
-        self.center = (int(left + (right - left) / 2), int(top + (bottom - top) / 2))
+
+        if not(top is None & bottom is None & left is None & right is None):
+            self.center = (int(left + (right - left) / 2), int(top + (bottom - top) / 2))
+
+    def __eq__(self, other):
+        return self.top == other & self.bottom == other & self.left == other & self.right == other & self.center == other
 
 
 class Group(object):
 
-    def __init__(self, top, bottom, left, right):
+    def __init__(self, top=None, bottom=None, left=None, right=None):
         self.points = 0
         self.position = Result(top, bottom, left, right)
+
+    def __eq__(self, other):
+        return self.position == other
+
+
+class Point(object):
+
+    def __init__(self, x, y):
+        self.x = x
+        self.y = y
--- modules/hough.py
+++ modules/hough.py
@@ -5,9 +5,6 @@ from modules.base import *
 
 
 def HoughTransf(src: np.ndarray, center, cfg: Config):
-
-    blured = src.copy()
-    cv2.bitwise_and(src, blured)
     blured = cv2.GaussianBlur(src, (7, 7), 2.0, 2.0)
 
     if cfg.gui:
@@ -26,83 +23,82 @@ def HoughTransf(src: np.ndarray, center, cfg: Config):
 
     intersections = list()
 
-    # if cfg.probabilistic:
-    # minLineLength = 50
-    # maxLineGap = 10
-    # lines = cv2.HoughLinesP(edges, 1, np.pi / 180, cfg.hough_threshold, minLineLength, maxLineGap)
+    if cfg.probabilistic:
+        minLineLength = 50
+        maxLineGap = 10
+        lines = cv2.HoughLinesP(edges, 1, np.pi / 180, cfg.hough_threshold, minLineLength, maxLineGap)
 
-        # for x in lines:
-        #     for y in lines:
-        #         intersections.append(computeIntersect(x, y))
-    #
-    # else:
+        for x in lines:
+            for y in lines:
+                intersections.append(computeIntersect(x, y))
 
-    lines = cv2.HoughLines(edges, 1, np.pi / 180, cfg.hough_threshold, 0, 0)
-    if cfg.gui:
-        hough = cv2.cvtColor(src, cv2.COLOR_GRAY2RGB)
-        for line in lines:
-            for rho, theta in line:
-                a = np.cos(theta)
-                b = np.sin(theta)
-                x0 = a * rho
-                y0 = b * rho
-                x1 = int(x0 + 1000 * (-b))
-                y1 = int(y0 + 1000 * a)
-                x2 = int(x0 - 1000 * (-b))
-                y2 = int(y0 - 1000 * a)
-                intersections.append((x0, y0))
-                cv2.line(hough, (x1, y1), (x2, y2), (0, 255, 0), 4, 8, 0)
-        cv2.imshow('hough', cv2.resize(hough, (300, 250)))
-        cv2.moveWindow('hough', 1050, 350)
+        if cfg.gui:
+            hough = cv2.cvtColor(src, cv2.COLOR_GRAY2RGB)
+            for line in lines:
+                for x1, y1, x2, y2 in line:
+                    cv2.line(hough, (x1, y1), (x2, y2), (0, 255, 0), 6)
+            cv2.imshow('hough', cv2.resize(hough, (300, 250)))
+            cv2.moveWindow('hough', 1050, 350)
 
-    # if cfg.gui:
-    #     hough = cv2.cvtColor(src, cv2.COLOR_GRAY2RGB)
-    #     for x1, y1, x2, y2 in lines[0]:
-    #         cv2.line(hough, (x1, y1), (x2, y2), (0, 255, 0), 6)
-    #     cv2.imshow('hough', hough)
-        # cv2.moveWindow('hough', 1000, 400)
+    else:
 
-    # for x in lines[0]:
-    #     for y in lines[0]:
-    #         if acceptLinePair(x, y, np.pi / 32):
-    #             intersections.append(computeIntersect(x, y))
+        lines = cv2.HoughLines(edges, 1, np.pi / 180, cfg.hough_threshold, 0, 0)
+        if cfg.gui:
+            hough = cv2.cvtColor(src, cv2.COLOR_GRAY2RGB)
+            for line in lines:
+                for nline in lines:
+                    if acceptLinePair(line, nline, np.pi / 32):
+                        intersections.append(computeIntersect(line, nline))
 
-    # if cfg.gui:
-    #     pedges = src.copy()
-    #
-    #     for x1, y1, x2, y2 in lines[0]:
-    #         cv2.line(pedges, (x1, y1), (x2, y2), (0, 255, 0), 2)
+                for rho, theta in line:
+                    a = np.cos(theta)
+                    b = np.sin(theta)
+                    x0 = a * rho
+                    y0 = b * rho
+                    x1 = int(x0 + 1000 * (-b))
+                    y1 = int(y0 + 1000 * a)
+                    x2 = int(x0 - 1000 * (-b))
+                    y2 = int(y0 - 1000 * a)
+                    cv2.line(hough, (x1, y1), (x2, y2), (0, 255, 0), 4, 8, 0)
+            cv2.imshow('hough', cv2.resize(hough, (300, 250)))
+            cv2.moveWindow('hough', 1050, 350)
 
-    gab_tolerance = 0
+    group_blocks = sortGroups(intersections, cfg.groups_min_distance)
+
+    result = Result()
+
+    for i in (0, cfg.max_gab_tolerance, 5):
+        if punctureFind(src, group_blocks, center, result, i):
+            break
+
+    group_blocks.clear()
+    return result
 
 
-def computeIntersect(line1, line2) -> list:
+def computeIntersect(line1, line2) -> Point:
     p1 = lineToPointPair(line1)
     p2 = lineToPointPair(line2)
 
-    x1 = p1[0].x
-    y1 = p1[0].y
-    x2 = p1[1].x
-    y2 = p1[1].y
-    x3 = p2[0].x
-    y3 = p2[0].y
-    x4 = p2[1].x
-    y4 = p2[1].y
+    x1 = p1[0][0]
+    y1 = p1[0][1]
+    x2 = p1[1][0]
+    y2 = p1[1][1]
+    x3 = p2[0][0]
+    y3 = p2[0][1]
+    x4 = p2[1][0]
+    y4 = p2[1][1]
 
     d = ((x1 - x2) * (y3 - y4)) - ((y1 - y2) * (x3 - x4))
 
-    pt = list()
-    pt[0] = ((x1 * y2 - y1 * x2) * (x3 - x4) - (x1 - x2) * (x3 * y4 - y3 * x4)) / d
-    pt[1] = ((x1 * y2 - y1 * x2) * (y3 - y4) - (y1 - y2) * (x3 * y4 - y3 * x4)) / d
-
-    return pt
+    return Point(((x1 * y2 - y1 * x2) * (x3 - x4) - (x1 - x2) * (x3 * y4 - y3 * x4)) / d,
+                 ((x1 * y2 - y1 * x2) * (y3 - y4) - (y1 - y2) * (x3 * y4 - y3 * x4)) / d)
 
 
 def lineToPointPair(line) -> list:
     points = list()
 
-    r = line[0]
-    t = line[1]
+    r = line[0][0]
+    t = line[0][1]
     cos_t = np.cos(t)
     sin_t = np.sin(t)
     x0 = r * cos_t
@@ -116,8 +112,8 @@ def lineToPointPair(line) -> list:
 
 
 def acceptLinePair(line1, line2, minTheta) -> bool:
-    theta1 = line1[1]
-    theta2 = line2[1]
+    theta1 = line1[0][1]
+    theta2 = line2[0][1]
 
     if theta1 < minTheta:
         theta1 += np.pi
@@ -126,3 +122,152 @@ def acceptLinePair(line1, line2, minTheta) -> bool:
         theta2 += np.pi
 
     return abs(theta1 - theta2) > minTheta
+
+
+def sortGroups(points: list, min_distance: float) -> list():
+    group_blocks = list()
+    groups = list()
+
+    points_index = 0
+    group_index = 0
+    in_group_index = 0
+
+    while len(points) != 0:
+        p = points[0]
+        g = Group(p.y, p.y, p.x, p.x)
+        g.points = 1
+
+        group_i = list()
+        groups.append(group_i)
+        groups[group_index].append(p)
+        points.pop(0)
+
+        in_group_index = 0
+
+        while in_group_index < len(groups[group_index]):
+            points_index = 0
+
+            while points_index < len(points):
+                p = points[points_index]
+
+                if pointsDistance(groups[group_index][in_group_index], p) <= min_distance:
+                    groups[group_index].append(points[points_index])
+
+                    if g.position.top > p.y:
+                        g.position.top = p.y
+                    if g.position.bottom < p.y:
+                        g.position.bottom = p.y
+                    if g.position.left > p.x:
+                        g.position.left = p.x
+                    if g.position.right < p.x:
+                        g.position.right = p.x
+
+                    g.points += 1
+                    points.pop(points_index)
+
+                else:
+                    points_index += 1
+
+            in_group_index += 1
+
+        group_blocks.append(g)
+        group_index += 1
+
+    return group_blocks
+
+
+def pointsDistance(p1: Point, p2: Point) -> float:
+    x = p1.x - p2.x
+    y = p1.y - p2.y
+    return np.sqrt(x**2 + y**2)
+
+
+def punctureFind(img: np.ndarray, group_blocks: list, center: Point, result: Result, gab_tolerance: float) -> bool:
+    if len(group_blocks) == 0:
+        return False
+
+    top_g = Group()
+    bottom_g = Group()
+    left_g = Group()
+    right_g = Group()
+
+    result.top = 0
+    result.bottom = 10 * center.y
+    result.left = 0
+    result.right = 10 * center.x
+
+    for g in group_blocks:
+        if g.position.left - gab_tolerance < center.x & g.position.right + gab_tolerance > center.x:
+            if g.position.bottom < center.y & g.position.bottom > result.top:
+                result.top = g.position.bottom
+                top_g = g
+
+            if g.position.top > center.y & g.position.top < result.bottom:
+                result.bottom = g.position.top
+                bottom_g = g
+
+        if g.position.top - gab_tolerance < center.y & g.position.bottom + gab_tolerance > center.y:
+            if g.position.right < center.x & g.position.right > result.left:
+                result.left = g.position.right
+                left_g = g
+
+            if g.position.left > center.x & g.position.left < result.right:
+                result.right = g.position.left
+                right_g = g
+
+    if top_g is not None & bottom_g is not None & left_g is not None & right_g is not None:
+        white_extension = 5
+
+        while whiteLineArea(img,
+                            Point(top_g.position.left - white_extension, result.top + 1),
+                            Point(top_g.position.right + white_extension, result.top + 1)):
+            result.top += 1
+
+        while whiteLineArea(img,
+                            Point(top_g.position.left - white_extension, result.top - 1),
+                            Point(top_g.position.right + white_extension, result.top - 1)):
+            result.bottom -= 1
+
+        while whiteLineArea(img,
+                            Point(result.left + 1, left_g.position.top - white_extension),
+                            Point(result.left + 1, left_g.position.bottom + white_extension)):
+            result.left += 1
+
+        while whiteLineArea(img,
+                            Point(result.right - 1, right_g.position.top - white_extension),
+                            Point(result.right - 1, right_g.position.bottom + white_extension)):
+            result.right -= 1
+
+        return True
+
+    else:
+        return False
+
+    return False
+
+
+def whiteLineArea(img: np.ndarray, fr: Point, to: Point) -> bool:
+    horizontal = fr.y == to.y
+
+    i_start = float
+    i_end = float
+    if horizontal:
+        i_start = 0 if fr.x < 0 else fr.x
+        i_end = img.shape[1] - 1 if to.x >= img.shape[1] else to.x
+
+    else:
+        i_start = 0 if fr.y < 0 else fr.y
+        i_end = img.shape[0] - 1 if to.y >= img.shape[0] else to.y
+
+    j = int(fr.y if horizontal else fr.x)
+
+    for i in (i_start, i_end, 1):
+        if horizontal:
+            if img.at(j, i) == 0:
+                return False
+
+        else:
+            if img.at(i, j) == 0:
+                return False
+
+    return True
