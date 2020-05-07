Feature. Imlement Convex Hull and compare two methods with simmetrizing. Hardness computation complete. Fix. SegFault on image *984.JPG. Add extra logical conditions. Get all final images from dataset.

main.py
@@ -2,40 +2,81 @@ import glob
 import cv2
 from config.config import Config
 from modules.preproces import detect_puncture
+from modules.hardness import computeHardness
+from plot.plot import *
 import numpy as np
+
+
+# =====================================================================================================================
 
 # Read configuration file
 config = Config('./cfg/config.yml')
 template = cv2.imread('./dataset/templates/' + config.puncture_tpl_file + '.jpg')
 
-# Read all images from subdirectory
-dataset = [cv2.imread(file) for file in glob.glob('./dataset/*.JPG')]
+# Read all image filenames from subdirectory
+dataset = [[cv2.imread(file), file.replace('./dataset/', '')] for file in glob.glob('./dataset/*.JPG')]
 
-# for img in dataset:
-# Get first image
-img = dataset[0]
-gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
-(thresh, binarized) = cv2.threshold(gray, 0, 255, cv2.THRESH_BINARY + cv2.THRESH_OTSU)
+for img, fname in dataset:
 
-res = detect_puncture(binarized, template, config)
+    if config.trace:
+        print('\n Processing ' + fname)
 
-if config.gui:
-    pimg = cv2.resize(img, (300, 250))
-    ptmp = cv2.resize(template, (300, 250))
-    pgray = cv2.resize(gray, (300, 250))
-    pbinarized = cv2.resize(binarized, (300, 250))
-    # pdenoised = cv2.resize(denoised, (300, 250))
+    img: np.ndarray = img
+    gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+    thresh, binarized = cv2.threshold(gray, 0, 255, cv2.THRESH_BINARY + cv2.THRESH_OTSU)
 
-    cv2.imshow('input', pimg)
-    cv2.moveWindow('input', 0, 0)
-    cv2.imshow('template', ptmp)
-    cv2.moveWindow('template', 350, 0)
-    cv2.imshow('grayscale', pgray)
-    cv2.moveWindow('grayscale', 700, 0)
-    cv2.imshow('binary', pbinarized)
-    cv2.moveWindow('binary', 1050, 0)
-    # cv2.imshow('denoised', pdenoised)
-    # cv2.moveWindow('denoised', 1300, 0)
+    if config.trace:
+        print('Binarized')
 
-    cv2.waitKey(0)
+    res, decision = detect_puncture(binarized, template, config)
+
+    hardness = None
+    if decision:
+        if config.trace:
+            print('Puncture detected')
+
+        hardness = computeHardness(res)
+
+        if config.trace:
+            print('Hardness computed')
+
+    if config.gui:
+        pimg = cv2.resize(img, (300, 250))
+        ptmp = cv2.resize(template, (300, 250))
+        pgray = cv2.resize(gray, (300, 250))
+        pbinarized = cv2.resize(binarized, (300, 250))
+
+        cv2.imshow('input', pimg)
+        cv2.moveWindow('input', 0, 0)
+        cv2.imshow('template', ptmp)
+        cv2.moveWindow('template', 350, 0)
+        cv2.imshow('grayscale', pgray)
+        cv2.moveWindow('grayscale', 700, 0)
+        cv2.imshow('binary', pbinarized)
+        cv2.moveWindow('binary', 1050, 0)
+
+    final = img.copy()
+    if decision:
+        cv2.rectangle(final, (int(res.left), int(res.top)), (int(res.right), int(res.bottom)), (0, 0, 0), 3, 8, 0)
+        DrawHardness(final, hardness)
+
+    nfname = './output/final_' + fname
+    cv2.imwrite(nfname, final)
+
+    if config.trace:
+        print('Image writed as ' + nfname + '\n')
+
+    if config.gui:
+        final = cv2.resize(final, (300, 250))
+        cv2.imshow('final', final)
+        cv2.moveWindow('final', 1050, 700)
+
+    if config.trace:
+        cv2.waitKey(0)
+
     cv2.destroyAllWindows()
+
+if config.trace:
+    print('Finish!')
+
+# =====================================================================================================================

modules/hough.py
@@ -4,7 +4,9 @@ from config.config import Config
 from modules.base import *
 
 
-def HoughTransf(src: np.ndarray, center, cfg: Config):
+# =====================================================================================================================
+def HoughTransf(src: np.ndarray, center: Point, cfg: Config) -> Result:
+
     blured = cv2.GaussianBlur(src, (7, 7), 2.0, 2.0)
 
     if cfg.gui:
@@ -21,13 +23,14 @@ def HoughTransf(src: np.ndarray, center, cfg: Config):
         cv2.imshow('edges', pedges)
         cv2.moveWindow('edges', 700, 350)
 
-    intersections = list()
+    intersections = []
 
     if cfg.probabilistic:
         minLineLength = 50
         maxLineGap = 10
         lines = cv2.HoughLinesP(edges, 1, np.pi / 180, cfg.hough_threshold, minLineLength, maxLineGap)
 
+        # compute the intersections of the detected lines
         for x in lines:
             for y in lines:
                 intersections.append(computeIntersect(x, y))
@@ -43,13 +46,17 @@ def HoughTransf(src: np.ndarray, center, cfg: Config):
     else:
 
         lines = cv2.HoughLines(edges, 1, np.pi / 180, cfg.hough_threshold, 0, 0)
+
+        # compute the intersections of the detected lines
+        for line in lines:
+            for nline in lines:
+                if acceptLinePair(line, nline, np.pi / 32):
+                    intersections.append(computeIntersect(line, nline))
+
         if cfg.gui:
             hough = cv2.cvtColor(src, cv2.COLOR_GRAY2RGB)
-            for line in lines:
-                for nline in lines:
-                    if acceptLinePair(line, nline, np.pi / 32):
-                        intersections.append(computeIntersect(line, nline))
 
+            for line in lines:
                 for rho, theta in line:
                     a = np.cos(theta)
                     b = np.sin(theta)
@@ -63,7 +70,8 @@ def HoughTransf(src: np.ndarray, center, cfg: Config):
             cv2.imshow('hough', cv2.resize(hough, (300, 250)))
             cv2.moveWindow('hough', 1050, 350)
 
-    group_blocks = sortGroups(intersections, cfg.groups_min_distance)
+    # filter and sort the intersections groups
+    group_blocks = sortGroups(intersections.copy(), cfg.groups_min_distance)
 
     result = Result()
 
@@ -71,10 +79,30 @@ def HoughTransf(src: np.ndarray, center, cfg: Config):
         if punctureFind(src, group_blocks, center, result, i):
             break
 
+    if cfg.gui:
+        inters = cv2.cvtColor(src, cv2.COLOR_GRAY2RGB)
+        for i in intersections:
+            cv2.circle(inters, (int(i.x), int(i.y)), 2, (255, 0, 0), 4)
+        for g in group_blocks:
+            cv2.rectangle(inters,
+                          (int(g.position.left), int(g.position.top)),
+                          (int(g.position.right), int(g.position.bottom)),
+                          (0, 0, 255), 2, 8)
+        cv2.line(inters, (int(center.x), int(center.y - 25)), (int(center.x), int(center.y + 25)), (0, 0, 255))
+        cv2.line(inters, (int(center.x - 25), int(center.y)), (int(center.x + 25), int(center.y)), (0, 0, 255))
+        cv2.rectangle(inters,
+                      (int(result.left), int(result.top)),
+                      (int(result.right), int(result.bottom)),
+                      (0, 255, 0), 1, 8, 0)
+        cv2.imshow('inters', cv2.resize(inters, (300, 250)))
+        cv2.moveWindow('inters', 1400, 350)
+
     group_blocks.clear()
     return result
+# =====================================================================================================================
 
 
+# =====================================================================================================================
 def computeIntersect(line1, line2) -> Point:
     p1 = lineToPointPair(line1)
     p2 = lineToPointPair(line2)
@@ -92,8 +120,10 @@ def computeIntersect(line1, line2) -> Point:
 
     return Point(((x1 * y2 - y1 * x2) * (x3 - x4) - (x1 - x2) * (x3 * y4 - y3 * x4)) / d,
                  ((x1 * y2 - y1 * x2) * (y3 - y4) - (y1 - y2) * (x3 * y4 - y3 * x4)) / d)
+# =====================================================================================================================
 
 
+# =====================================================================================================================
 def lineToPointPair(line) -> list:
     points = list()
 
@@ -109,8 +139,10 @@ def lineToPointPair(line) -> list:
     points.append((x0 - alpha * (- sin_t), y0 - alpha * cos_t))
 
     return points
+# =====================================================================================================================
 
 
+# =====================================================================================================================
 def acceptLinePair(line1, line2, minTheta) -> bool:
     theta1 = line1[0][1]
     theta2 = line2[0][1]
@@ -122,9 +154,13 @@ def acceptLinePair(line1, line2, minTheta) -> bool:
         theta2 += np.pi
 
     return abs(theta1 - theta2) > minTheta
+# =====================================================================================================================
 
 
+# =====================================================================================================================
 def sortGroups(points: list, min_distance: float) -> list():
+
+    # sort the lines intersections groups into Groups
     group_blocks = list()
     groups = list()
 
@@ -149,6 +185,7 @@ def sortGroups(points: list, min_distance: float) -> list():
 
             while points_index < len(points):
                 p = points[points_index]
+                # add points in minimum range distance to current group
 
                 if pointsDistance(groups[group_index][in_group_index], p) <= min_distance:
                     groups[group_index].append(points[points_index])
@@ -174,15 +211,21 @@ def sortGroups(points: list, min_distance: float) -> list():
         group_index += 1
 
     return group_blocks
+# =====================================================================================================================
 
 
+# =====================================================================================================================
 def pointsDistance(p1: Point, p2: Point) -> float:
     x = p1.x - p2.x
     y = p1.y - p2.y
     return np.sqrt(x**2 + y**2)
+# =====================================================================================================================
 
 
+# =====================================================================================================================
 def punctureFind(img: np.ndarray, group_blocks: list, center: Point, result: Result, gab_tolerance: float) -> bool:
+
+    # Find the puncture position by center and intersections groups
     if len(group_blocks) == 0:
         return False
 
@@ -191,63 +234,70 @@ def punctureFind(img: np.ndarray, group_blocks: list, center: Point, result: Res
     left_g = Group()
     right_g = Group()
 
-    result.top = 0
+    result.top = 0.0
     result.bottom = 10 * center.y
-    result.left = 0
+    result.left = 0.0
     result.right = 10 * center.x
+    result.center = Point(0.0, 0.0)
 
     for g in group_blocks:
-        if g.position.left - gab_tolerance < center.x & g.position.right + gab_tolerance > center.x:
-            if g.position.bottom < center.y & g.position.bottom > result.top:
+        if (g.position.left - gab_tolerance < center.x) and (g.position.right + gab_tolerance > center.x):
+            if (g.position.bottom < center.y) and (g.position.bottom > result.top):
                 result.top = g.position.bottom
                 top_g = g
 
-            if g.position.top > center.y & g.position.top < result.bottom:
+            if (g.position.top > center.y) and (g.position.top < result.bottom):
                 result.bottom = g.position.top
                 bottom_g = g
 
-        if g.position.top - gab_tolerance < center.y & g.position.bottom + gab_tolerance > center.y:
-            if g.position.right < center.x & g.position.right > result.left:
+        if (g.position.top - gab_tolerance < center.y) and (g.position.bottom + gab_tolerance > center.y):
+            if (g.position.right < center.x) and (g.position.right > result.left):
                 result.left = g.position.right
                 left_g = g
 
-            if g.position.left > center.x & g.position.left < result.right:
+            if (g.position.left > center.x) and (g.position.left < result.right):
                 result.right = g.position.left
                 right_g = g
 
-    if top_g is not None & bottom_g is not None & left_g is not None & right_g is not None:
+    if top_g.nis_none() and bottom_g.nis_none() and left_g.nis_none() and right_g.nis_none():
         white_extension = 5
 
-        while whiteLineArea(img,
-                            Point(top_g.position.left - white_extension, result.top + 1),
-                            Point(top_g.position.right + white_extension, result.top + 1)):
-            result.top += 1
+        if top_g.position.left is not None and bottom_g.position.right is not None:
+            while whiteLineArea(img,
+                                Point(top_g.position.left - white_extension, result.top + 1),
+                                Point(top_g.position.right + white_extension, result.top + 1)):
+                result.top += 1
 
-        while whiteLineArea(img,
-                            Point(top_g.position.left - white_extension, result.top - 1),
-                            Point(top_g.position.right + white_extension, result.top - 1)):
-            result.bottom -= 1
+        if bottom_g.position.left is not None and bottom_g.position.right is not None:
+            while whiteLineArea(img,
+                                Point(bottom_g.position.left - white_extension, result.bottom - 1),
+                                Point(bottom_g.position.right + white_extension, result.bottom - 1)):
+                result.bottom -= 1
 
-        while whiteLineArea(img,
-                            Point(result.left + 1, left_g.position.top - white_extension),
-                            Point(result.left + 1, left_g.position.bottom + white_extension)):
-            result.left += 1
+        if left_g.position.top is not None and left_g.position.bottom is not None:
+            while whiteLineArea(img,
+                                Point(result.left + 1, left_g.position.top - white_extension),
+                                Point(result.left + 1, left_g.position.bottom + white_extension)):
+                result.left += 1
 
-        while whiteLineArea(img,
-                            Point(result.right - 1, right_g.position.top - white_extension),
-                            Point(result.right - 1, right_g.position.bottom + white_extension)):
-            result.right -= 1
+        if right_g.position.top is not None and right_g.position.bottom is not None:
+            while whiteLineArea(img,
+                                Point(result.right - 1, right_g.position.top - white_extension),
+                                Point(result.right - 1, right_g.position.bottom + white_extension)):
+                result.right -= 1
 
         return True
 
     else:
         return False
+# =====================================================================================================================
 
-    return False
 
-
+# =====================================================================================================================
 def whiteLineArea(img: np.ndarray, fr: Point, to: Point) -> bool:
-    horizontal = fr.y == to.y
+
+    # check for white space between two points
+    horizontal = int(fr.y) == int(to.y)
 
     i_start = float
     i_end = float
@@ -261,13 +311,17 @@ def whiteLineArea(img: np.ndarray, fr: Point, to: Point) -> bool:
 
     j = int(fr.y if horizontal else fr.x)
 
-    for i in (i_start, i_end, 1):
-        if horizontal:
-            if img.at(j, i) == 0:
-                return False
+    if 0 <= j < img.shape[0]:
+        for i in range(int(i_start), int(i_end)):
+            if horizontal:
+                if img.item((j, i)) == 0:
+                    el = img.item((j, i))
+                    return False
 
-        else:
-            if img.at(i, j) == 0:
-                return False
+            else:
+                if img.item((i, j)) == 0:
+                    el = img.item((i, j))
+                    return False
 
     return True
+# =====================================================================================================================

modules/preproces.py
@@ -1,15 +1,30 @@
 import cv2
 import numpy as np
 from config.config import Config
-from modules.hough import HoughTransf
-from modules.base import Result
+from modules.hough import *
+from modules.convex_hull import *
+from modules.base import *
+from modules.evaluate import *
 
 
-def detect_puncture(src: np.ndarray, template: np.ndarray, cfg: Config):
+# =====================================================================================================================
+def detect_puncture(src: np.ndarray, template: np.ndarray, cfg: Config) -> [Result, bool]:
+
+    # Preproccessing
     tmp = src.copy()
     denoise(tmp, cv2.MORPH_OPEN, cfg)
-    res = detect_by_template(tmp, template, cfg)
 
+    # Detect the puncture center
+    center_result: Result = detect_by_template(tmp, template, cfg)
+
+    if cfg.trace:
+        print('Template matched')
+
+    center = center_result.center
+    result_hough = Result()
+    result_convex = Result()
+
+    # Fill the center star
     denoise(tmp, cv2.MORPH_CLOSE, cfg)
     kernel = cv2.getStructuringElement(cfg.kernel_shape, (cfg.center_fill_size, cfg.center_fill_size))
     tmp = cv2.erode(tmp, kernel, 5)
@@ -17,9 +32,13 @@ def detect_puncture(src: np.ndarray, template: np.ndarray, cfg: Config):
     h, w = tmp.shape[:2]
     mask = np.zeros((h + 2, w + 2), np.uint8)
 
-    cv2.floodFill(tmp, mask, res.center, 0)
+    cv2.floodFill(tmp, mask, (int(center.x), int(center.y)), 0)
 
+    # Fill small areas (remove possible inner lines)
     denoise(tmp, cv2.MORPH_CLOSE, cfg)
+
+    if cfg.trace:
+        print('Denoised')
 
     if cfg.gui:
         pdenoised = cv2.cvtColor(tmp, cv2.COLOR_GRAY2RGB)
@@ -27,22 +46,58 @@ def detect_puncture(src: np.ndarray, template: np.ndarray, cfg: Config):
         cv2.imshow('punctured', pdenoised)
         cv2.moveWindow('punctured', 0, 350)
 
-    HoughTransf(tmp, res.center, cfg)
+    # Hough + convex hull method
+    result_hough = HoughTransf(tmp, center_result.center, cfg)
 
-    return res
+    if cfg.trace:
+        print('Hough Transformed')
+
+    result_convex = ConvexHullMethod(tmp, center, cfg)
+
+    if cfg.trace:
+        print('Convex Hulled')
+
+    result = Result()
+
+    if not resultCompare(center, result_hough, result_convex, result, cfg.result_compare_tolerance):
+        if result_hough.is_none():
+            result = result_convex
+        elif result_convex.is_none():
+            result = result_hough
+        elif result.is_none():
+            result = result_convex
+
+    symmetrize(center, result, cfg.symmetrize_tolerance)
+
+    if cfg.trace:
+        print('Symmetrized')
+
+    return [result, True]
+# =====================================================================================================================
 
 
+# =====================================================================================================================
 def denoise(src: np.ndarray, operation: int, cfg: Config):
+    # invert image
     cv2.bitwise_not(src, src)
     kernel = cv2.getStructuringElement(cfg.kernel_shape, (cfg.kernel_size, cfg.kernel_size))
     cv2.morphologyEx(src, operation, kernel, src)
+    # return image invertion back to normal
     cv2.bitwise_not(src, src)
+# =====================================================================================================================
 
 
+# =====================================================================================================================
 def detect_by_template(src: np.ndarray, template: np.ndarray, cfg: Config) -> Result:
+
+    # Detect the template position
     img = template_detect(src, template, cfg.match_method)
+
+    # Localize the best match position
     minVal, maxVal, minLoc, maxLoc = cv2.minMaxLoc(img)
     matchLoc = maxLoc
+
+    # For SQDIFF and SQDIFF_NORMED, the best matches are lower values.
     if cfg.match_method == cv2.TM_SQDIFF or cfg.match_method == cv2.TM_SQDIFF_NORMED:
         matchLoc = minLoc
 
@@ -61,10 +116,15 @@ def detect_by_template(src: np.ndarray, template: np.ndarray, cfg: Config) -> Re
         cv2.moveWindow('centered', 1400, 0)
 
     return Result(matchLoc[1], matchLoc[1] + template.shape[0], matchLoc[0], matchLoc[0] + template.shape[1])
+# =====================================================================================================================
 
 
+# =====================================================================================================================
 def template_detect(src: np.ndarray, template: np.ndarray, match_method: int):
+
+    # Do the matching and normalize result
     res = cv2.matchTemplate(cv2.cvtColor(src, cv2.COLOR_GRAY2RGB), template, match_method)
     cv2.normalize(res, res, 0, 1, cv2.NORM_MINMAX, - 1)
 
     return res
+# =====================================================================================================================