920e3039884917bd2224b12cae869e40af07bb4c
Dmitrii Leliuhin <dleliuhin@gmail.com>
2020-05-08 10:44:23 +0300

    Rebase. Add hw_detector function to modules. Hiding the interface from the user.

--- test_michal-py/TEST.md
+++ test_michal-py/TEST.md
@@ -7,12 +7,21 @@ All tests are in the directory *michal-py/test_michal-py*.
 
 ## Unit
 
-In progress.
+- Case 1. Test specific image. We set file name<br/> 
+```fname = 'MwPic_30_1_2015__16_1__29_407.JPG'``` after that imread:<br/>
+```img = cv2.imread('./dataset/' + fname)``` and put into main module function:<br/>
+```final, d1, d2, hw = hw_detector(img, fname, template, config)```
+Exelent we get an annotated image, two bounding box diagonals and calculated hardness. 
+Unit test is passed when template and input images sizes not empty, 
+and output bounding box parameters are greater than 0.
 
 Start unit tests:<br />
 ```
 cd michal-py
 ./scripts/test.sh
 ```
+or using PyCharmIDE:<br/>
+
+```*Run->* 'test.py':```
 
 ---
--- test_michal-py/test.py
+++ test_michal-py/test.py
@@ -1,13 +1,48 @@
+import glob
+import cv2
 import sys
 import unittest
 
 sys.path.insert(1, "../")
+sys.path.insert(1, "../modules")
+
+from modules.hw_detector import *
 
 
-class MyTestCase(unittest.TestCase):
-    def test_something(self):
-        self.assertEqual(True, True)
+# =====================================================================================================================
+class MyTestCases(unittest.TestCase):
+
+    def test_one_image(self):
+
+        config = Config('../cfg/config.yml')
+
+        # No need to draw or trace in Unit test
+        config.trace = False
+        config.gui = False
+
+        template = cv2.imread('../dataset/templates/' + config.puncture_tpl_file + '.jpg')
+
+        self.assertEqual(template.size > 0, True)
+
+        fname = 'MwPic_30_1_2015__16_1__29_407.JPG'
+
+        self.assertEqual(len(fname) > 0, True)
+
+        img = cv2.imread('../dataset/' + fname)
+
+        self.assertEqual(img.size > 0, True)
+
+        final, d1, d2, hw = hw_detector(img, fname, template, config)
+
+        self.assertEqual(d1 > 0, True)
+        self.assertEqual(d2 > 0, True)
+        self.assertEqual(hw > 0, True)
+        self.assertEqual(final.size > 0, True)
+
+# =====================================================================================================================
 
 
+# =====================================================================================================================
 if __name__ == '__main__':
     unittest.main()
+# =====================================================================================================================
